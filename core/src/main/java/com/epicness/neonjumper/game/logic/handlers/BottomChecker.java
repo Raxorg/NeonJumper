8267dadafc285eaec777252eed672692028a1b9b
Groxar <lrdolucho@gmail.com>
2024-11-15 20:23:36 -0400

    Progress

--- core/src/main/java/com/epicness/neonjumper/game/logic/handlers/BottomChecker.java
+++ core/src/main/java/com/epicness/neonjumper/game/logic/handlers/BottomChecker.java
@@ -1,21 +1,25 @@
 package com.epicness.neonjumper.game.logic.handlers;
 
 import static com.epicness.neonjumper.game.constants.GameConstants.PLAYER_GLOW_SIZE;
+import static com.epicness.neonjumper.game.logic.handlers.CameraHandler.gameHeight;
 
+import com.badlogic.gdx.graphics.Camera;
 import com.epicness.neonjumper.game.logic.GameLogicHandler;
 import com.epicness.neonjumper.game.stuff.Player;
 
 public class BottomChecker extends GameLogicHandler {
 
     private Player player;
+    private Camera camera;
 
     @Override
     protected void init() {
         player = stuff.getPlayer();
+        camera = screen.getDynamicCamera();
     }
 
     public void checkBottom() {
-        if (player.getY() < -PLAYER_GLOW_SIZE) {
+        if (player.getY() < camera.position.y - gameHeight() / 2f - PLAYER_GLOW_SIZE) {
             player.speed.setZero();
             get(PlayerSpawner.class).spawnPlayer();
         }
--- core/src/main/java/com/epicness/neonjumper/game/logic/handlers/PlayerSpawner.java
+++ core/src/main/java/com/epicness/neonjumper/game/logic/handlers/PlayerSpawner.java
@@ -1,22 +1,27 @@
 package com.epicness.neonjumper.game.logic.handlers;
 
 import static com.epicness.neonjumper.game.constants.GameConstants.PLAYER_GLOW_SIZE;
+import static com.epicness.neonjumper.game.logic.handlers.CameraHandler.gameHeight;
 import static com.epicness.neonjumper.game.logic.handlers.CameraHandler.gameWidth;
 
+import com.badlogic.gdx.graphics.Camera;
 import com.epicness.neonjumper.game.logic.GameLogicHandler;
 import com.epicness.neonjumper.game.stuff.Player;
 
 public class PlayerSpawner extends GameLogicHandler {
 
     private Player player;
+    private Camera camera;
 
     @Override
     protected void init() {
         player = stuff.getPlayer();
         player.setX(gameWidth() / 2f - PLAYER_GLOW_SIZE / 2f);
+
+        camera = screen.getDynamicCamera();
     }
 
     public void spawnPlayer() {
-        player.setY(200f);
+        player.setY(camera.position.y - gameHeight() / 2f + 200f);
     }
 }
